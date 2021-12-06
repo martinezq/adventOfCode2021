f050d4c8952f6d1e73afc289573d37b7ab5720e6
Marcin Koch <marcin.koch@tacton.com>
2021-12-06 19:07:01 +0100

    days 01 - 06

---
+++ day06/main01.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const R = require('ramda');
+
+const file = fs.readFileSync('input.txt').toString();
+const initialAges = file.split(',').map(Number);
+
+let ages = initialAges;
+
+console.log(ages);
+
+R.times(i => {
+    ages = nextDay(ages);
+    console.log(i, ages);
+}, 80);
+
+function nextDay(ages) {
+    ages = ages.map(a => a - 1);
+    const zeros = ages.filter(a => a < 0);
+    ages = ages.map(a => a < 0 ? 6 : a);
+
+    return ages.concat(zeros.map(z => 8));
+}
+
+console.log(ages.length)
---
+++ day06/main02.js
@@ -0,0 +1,31 @@
+const { count } = require('console');
+const fs = require('fs');
+const R = require('ramda');
+
+const file = fs.readFileSync('input.txt').toString();
+const initialAges = file.split(',').map(Number);
+
+const groups = R.groupBy(R.identity, initialAges);
+const keys = R.keys(groups);
+
+let ages = Array.from({length: 9}, (_, i) => i).map(age => groups[age] ? groups[age].length : 0)
+
+console.log(-1, JSON.stringify(ages));
+
+R.times(i => {
+    ages = nextDay(ages);
+    console.log(i, JSON.stringify(ages), ages.reduce((p, c) => p + c, 0));
+}, 256);
+
+function nextDay(ages) {
+    const dead = R.head(ages);
+    ages = ages.map((_, i) => ages[i+1]);
+    ages[8] = dead;
+    ages[6] += dead;
+
+    return ages;
+}
+
+// const total = ages.reduce((p, c) => p + c, 0);
+
+// console.log(total)
