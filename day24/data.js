5a429b7232b45226d3f9460522925a4434bb0f96
Marcin Koch <marcin.koch@tacton.com>
2021-12-24 11:45:32 +0100

    day24

---
+++ day24/data.js
@@ -0,0 +1,280 @@
+let a=0; w=0; x=0, y=0, z=0
+
+w = input[0];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 1);
+x = x + 13;
+x = x === w;
+x = x === 0;
+
+// z = 0, x = 1
+
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+
+// z = 0, y = 26
+
+y = y * 0;
+y = y + w;
+y = y + 3;
+y = y * x;
+
+// y = w + 4
+
+z = z + y;
+
+// z = w + 4
+
+w = input[1];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 1);
+x = x + 11;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 12;
+y = y * x;
+z = z + y;
+
+w = input[2];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 1);
+x = x + 15;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 9;
+y = y * x;
+z = z + y;
+
+w = input[3];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 26);
+x = x + -6;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 12;
+y = y * x;
+z = z + y;
+
+w = input[4];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 1);
+x = x + 15;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 2;
+y = y * x;
+z = z + y;
+
+w = input[5];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 26);
+x = x + -8;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 1;
+y = y * x;
+z = z + y;
+
+w = input[6];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 26);
+x = x + -4;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 1;
+y = y * x;
+z = z + y;
+
+w = input[7];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 1);
+x = x + 15;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 13;
+y = y * x;
+z = z + y;
+
+w = input[8];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 1);
+x = x + 10;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 1;
+y = y * x;
+z = z + y;
+
+w = input[9];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 1);
+x = x + 11;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 6;
+y = y * x;
+z = z + y;
+
+w = input[10];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 26);
+x = x + -11;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 2;
+y = y * x;
+z = z + y;
+
+w = input[11];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 26);
+x = x + 0;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 11;
+y = y * x;
+z = z + y;
+
+w = input[12];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 26);
+x = x + -8;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 10;
+y = y * x;
+z = z + y;
+
+w = input[13];
+x = x * 0;
+x = x + z;
+x = x % 26;
+z = Math.floor(z / 26);
+x = x + -7;
+x = x === w;
+x = x === 0;
+y = y * 0;
+y = y + 25;
+y = y * x;
+y = y + 1;
+z = z * y;
+y = y * 0;
+y = y + w;
+y = y + 3;
+y = y * x;
+z = z + y;
+
+return z
---
+++ day24/main01.js
@@ -0,0 +1,160 @@
+const R = require('ramda');
+const U = require('./utils');
+
+let best = Number.POSITIVE_INFINITY;
+let cache = {};
+
+U.runWrapper(run);
+
+// --------------------------------------------
+
+function run(lines) {
+    const data = lines.map(l => l);
+
+    const data1 = data.map(l => l.match(/([a-z]+)\s([a-z])(\s(.*))?/))
+    const commands = data1.map(c => ({
+        code: c[1],
+        left: c[2],
+        right: Number(c[4]) || c[4]
+    }));
+
+    let codes = [];
+    let buf = [];
+
+    commands.forEach(cmd => {
+        if (cmd.code === 'inp') {
+            if (buf.length > 0) {
+                codes.push(buf);
+            }
+            buf = [cmd];
+        } else {
+            buf.push(cmd);
+        }
+    });
+
+    codes.push(buf);
+
+    // U.log(data, commands);
+
+    const funcs = codes.map(code => {
+        const func = compile(code);
+        return func;
+    });
+
+    const result = solve(funcs);
+
+    // return result;
+
+}
+
+function solve(funcs, level, presult, value) {
+    value = value || '';
+    level = level || 0;
+    presult = presult || 0;
+    
+    if (level > 13) {
+        if (presult < best) {
+            best = presult;
+            U.log(value, level, presult);
+        }
+        return;
+    }
+
+    const func = funcs[level];
+
+    for (let i=9; i>=0; i--) {
+        const fresult = func(i, presult);
+
+        if (fresult > 1000000) continue;
+
+        solve(funcs, level + 1, fresult, value + i);
+    }
+
+}
+
+
+function compile(commands) {
+    let code = [
+        'let w=0; x=0, y=0, z = zin !== undefined ? zin : 0;'
+    ];
+
+    commands.forEach(cmd => {
+
+        switch (cmd.code) {
+            case 'inp':
+                // code.push(`console.log(${inputIndex}, w, x, y, z);`);
+                code.push(`${cmd.left} = digit;`);
+                break;
+            case 'add':
+                code.push(`${cmd.left} = ${cmd.left} + ${cmd.right};`);
+                break;
+            case 'mul':
+                code.push(`${cmd.left} = ${cmd.left} * ${cmd.right};`);
+                break;
+            case 'div':
+                code.push(`${cmd.left} = Math.floor(${cmd.left} / ${cmd.right});`);
+                break;
+            case 'mod':
+                code.push(`${cmd.left} = ${cmd.left} % ${cmd.right};`);
+                break;
+            case 'eql':
+                code.push(`${cmd.left} = ${cmd.left} === ${cmd.right} ? 1 : 0;`)
+                break;
+        }
+
+        // U.log(cmd, memory, input, inputIndex);
+    });
+
+    // code.push('console.log(input, w, x, y, z);');
+    code.push('return z');
+
+    return Function('digit', 'zin', code.join('\n'));
+}
+
+function compile2(commands) {
+    let code = [
+        'let w=0; x=0, y=0, z=0;'
+    ];
+
+    let inputIndex = 0;
+
+    commands.forEach(cmd => {
+
+        // const decodedRight = typeof cmd.right === 'number' ? cmd.right : memory[cmd.right];
+
+        switch (cmd.code) {
+            case 'inp':
+                //memory[cmd.left] = input[inputIndex++];
+                // code.push(`console.log(${inputIndex}, w, x, y, z);`);
+                code.push(`${cmd.left} = input[${inputIndex++}];`);
+                break;
+            case 'add':
+                // memory[cmd.left] = memory[cmd.left] + decodedRight;
+                code.push(`${cmd.left} = ${cmd.left} + ${cmd.right};`);
+                break;
+            case 'mul':
+                // memory[cmd.left] = memory[cmd.left] * decodedRight;
+                code.push(`${cmd.left} = ${cmd.left} * ${cmd.right};`);
+                break;
+            case 'div':
+                // memory[cmd.left] = Math.floor(memory[cmd.left] / decodedRight);
+                code.push(`${cmd.left} = Math.floor(${cmd.left} / ${cmd.right});`);
+                break;
+            case 'mod':
+                // memory[cmd.left] = memory[cmd.left] % decodedRight;
+                code.push(`${cmd.left} = ${cmd.left} % ${cmd.right};`);
+                break;
+            case 'eql':
+                // memory[cmd.left] = (memory[cmd.left] === decodedRight) ? 1 : 0
+                code.push(`${cmd.left} = ${cmd.left} === ${cmd.right} ? 1 : 0;`)
+                break;
+        }
+
+        // U.log(cmd, memory, input, inputIndex);
+    });
+
+    // code.push('console.log(input, w, x, y, z);');
+    code.push('return z');
+
+    return Function('input', code.join('\n'));
+}
